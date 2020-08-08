1352fd0971a6ebcdd2b94b1a00cad3b57bdd6833
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2020-08-08 10:59:12 +0100

    Added versioneer changes to repo
    Commiting generated changes as per
    https://github.com/warner/python-versioneer/blob/master/README.md

--- src/hsd/__init__.py
+++ src/hsd/__init__.py
@@ -11,3 +11,7 @@ Central module for the hsd package
 from .dump import dump, dumps
 from .parser import HsdParser
 from .dictbuilder import HsdDictBuilder
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
